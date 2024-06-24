4228eaf6a860cae849e58a15a7f1e558be9fbbca
Lucas Rocha <lucas.rocha@vexpenses.com>
2024-06-24 20:56:15 -0300

    incorrect code yet

--- workout_api/atleta/controller.py
+++ workout_api/atleta/controller.py
@@ -1,17 +1,24 @@
 from datetime import datetime
 from uuid import uuid4
-from fastapi import APIRouter, Body, HTTPException, status
-from pydantic import UUID4
+from fastapi import APIRouter, Body, HTTPException, Query, status
+from pydantic import UUID4, BaseModel
+from typing import Optional
+from sqlalchemy.exc import IntegrityError
 
 from workout_api.atleta.schemas import AtletaIn, AtletaOut, AtletaUpdate
 from workout_api.atleta.models import AtletaModel
 from workout_api.categorias.models import CategoriaModel
 from workout_api.centro_treinamento.models import CentroTreinamentoModel
-
 from workout_api.contrib.dependencies import DatabaseDependency
 from sqlalchemy.future import select
+from fastapi_pagination import Page, paginate, add_pagination
 
 router = APIRouter()
+
+class AtletaCustomOut(BaseModel):
+    nome: str
+    centro_treinamento: str
+    categoria: str
 
 @router.post(
     '/', 
@@ -54,6 +61,11 @@ async def post(
         
         db_session.add(atleta_model)
         await db_session.commit()
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_303_SEE_OTHER, 
+            detail=f'Já existe um atleta cadastrado com o cpf: {atleta_in.cpf}'
+        )
     except Exception:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
@@ -62,18 +74,44 @@ async def post(
 
     return atleta_out
 
-
 @router.get(
     '/', 
     summary='Consultar todos os Atletas',
     status_code=status.HTTP_200_OK,
-    response_model=list[AtletaOut],
+    response_model=Page[AtletaCustomOut],
 )
-async def query(db_session: DatabaseDependency) -> list[AtletaOut]:
-    atletas: list[AtletaOut] = (await db_session.execute(select(AtletaModel))).scalars().all()
+async def query(
+    db_session: DatabaseDependency,
+    nome: Optional[str] = Query(None, description="Nome do Atleta"),
+    cpf: Optional[str] = Query(None, description="CPF do Atleta")
+) -> Page[AtletaCustomOut]:
+    query = select(AtletaModel)
     
-    return [AtletaOut.model_validate(atleta) for atleta in atletas]
-
+    if nome:
+        query = query.filter(AtletaModel.nome.ilike(f'%{nome}%'))
+    if cpf:
+        query = query.filter(AtletaModel.cpf == cpf)
+    
+    atletas: list[AtletaModel] = (await db_session.execute(query)).scalars().all()
+    
+    # Constrói a resposta personalizada
+    response = []
+    for atleta in atletas:
+        centro_treinamento = (await db_session.execute(
+            select(CentroTreinamentoModel).filter_by(id=atleta.centro_treinamento_id))
+        ).scalars().first()
+        
+        categoria = (await db_session.execute(
+            select(CategoriaModel).filter_by(id=atleta.categoria_id))
+        ).scalars().first()
+        
+        response.append(AtletaCustomOut(
+            nome=atleta.nome,
+            centro_treinamento=centro_treinamento.nome if centro_treinamento else None,
+            categoria=categoria.nome if categoria else None
+        ))
+    
+    return paginate(response)
 
 @router.get(
     '/{id}', 
@@ -93,7 +131,6 @@ async def get(id: UUID4, db_session: DatabaseDependency) -> AtletaOut:
         )
     
     return atleta
-
 
 @router.patch(
     '/{id}', 
@@ -121,7 +158,6 @@ async def patch(id: UUID4, db_session: DatabaseDependency, atleta_up: AtletaUpda
 
     return atleta
 
-
 @router.delete(
     '/{id}', 
     summary='Deletar um Atleta pelo id',
@@ -140,3 +176,5 @@ async def delete(id: UUID4, db_session: DatabaseDependency) -> None:
     
     await db_session.delete(atleta)
     await db_session.commit()
+
+add_pagination(router)
